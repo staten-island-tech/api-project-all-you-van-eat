41a479a61c38fa658c67ec06fe0a4ed1c79de3b0
IvanZ505 <ivan.zheng22@sitechhs.com>
2021-05-11 10:51:31 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "b9b841dbb44044dd9d0d26c2b377cec6";
+
+GET "https://api.spoonacular.com/food/menuItems/search=burger&number=2&apiKey=" + key
