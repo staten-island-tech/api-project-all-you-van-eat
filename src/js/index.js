53a87db2eab12cf257d60a352998236fc22a37ea
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-05-16 14:24:49 -0400

    completed
    The code now displays images from the API however, apparently this API has a limit on how many calls you can make. per day unless you pay.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-//import { DOMSelectors } from "./DOM";
-//import { genres } from "./genre";
+import { DOMSelectors } from "./DOM";
+import { genres } from "./genre";
 
-//GET "https://api.spoonacular.com/food/menuItems/search=burger&number=2&apiKey=" + key
+const key = "b9b841dbb44044dd9d0d26c2b377cec6";
 
 const query = async function () {
   try {
@@ -10,9 +10,18 @@ const query = async function () {
     );
     const data = await response.json();
     console.log(data.menuItems);
-    data.menuItems.forEach((recipe) => {});
+    data.menuItems.forEach((recipe) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<section class="menu-grid">
+            <div class="menu-picture">
+                <img src="https://images.spoonacular.com/file/wximages/${recipe.id}-90x90.${recipe.imageType}" alt="" class="picture">
+            </div>
+        </section>
+        `
+      );
+    });
   } catch (error) {
-    console.log(error);
     alert("Hey, something went wrong!");
   }
 };
