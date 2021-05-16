502d4fe243dda7b0ff4589718941e8a049832c88
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-05-16 13:25:26 -0400

    cccc

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
+//import { DOMSelectors } from "./DOM";
+//import { genres } from "./genre";
 
-const key = "b9b841dbb44044dd9d0d26c2b377cec6";
+//GET "https://api.spoonacular.com/food/menuItems/search=burger&number=2&apiKey=" + key
 
-GET "https://api.spoonacular.com/food/menuItems/search=burger&number=2&apiKey=" + key
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/food/menuItems/search?query=All&number=15&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+    );
+    const data = await response.json();
+    console.log(data.menuItems);
+    data.menuItems.forEach((recipe) => {});
+  } catch (error) {
+    console.log(error);
+    alert("Hey, something went wrong!");
+  }
+};
+
+query();
