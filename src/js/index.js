2e76b7d255c937f5469097ac5bc83af3432a717e
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-05-17 10:09:37 -0400

    cc

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ const key = "b9b841dbb44044dd9d0d26c2b377cec6";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/food/menuItems/search?query=All&number=15&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+      `https://api.spoonacular.com/food/menuItems/search?query=All&number=50&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
     );
     const data = await response.json();
     console.log(data.menuItems);
@@ -14,7 +14,7 @@ const query = async function () {
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         `<section class="menu-grid">
-            <div class="menu-picture">
+            <div class="food-picture">
                 <img src="https://images.spoonacular.com/file/wximages/${recipe.id}-312x231.${recipe.imageType}" alt="" class="picture">
             </div>
         </section>
