ef4fdc573ef35f5259180655354ffd385639c249
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-05-24 13:23:36 -0400

    added more api results

--- src/js/index.js
+++ src/js/index.js
@@ -6,28 +6,26 @@ import { genres } from "./genre";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/food/menuItems/search?query=All&number=50&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+      `https://api.spoonacular.com/food/menuItems/search?query=All&number=60&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
     );
     const data = await response.json();
     console.log(data.menuItems);
     data.menuItems.forEach((recipe) => {
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
-        `<section class="menu-grid">   
-         <div class="food-card-front">
-                    <img src="${recipe.image}" alt="" class="picture">
+        `<div class="food-card">
+        <div class="food-card-front">
+                    <img src="${recipe.image}" alt="" class="picture"/>
                 </div>
 
             <div class="food-card-back">
                 <h3 class="food-title">${recipe.title}</h3>
-                <div class="chain-box">
+                <div class="chain-container">
                     <p class="restaurant-chain">Restaurant Chain:</p>
                     <p class="restaurant-chain">${recipe.restaurantChain}</p>
                 </div>
             </div>
-
         </div>   
-        </section>
         `
       );
     });
