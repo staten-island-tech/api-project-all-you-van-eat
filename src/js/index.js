7107833d08caf94718b71f1bc31b1029d48648c0
IvanZ505 <ivan.zheng22@sitechhs.com>
2021-05-27 09:12:29 -0400

    added search bar

--- src/js/index.js
+++ src/js/index.js
@@ -30,6 +30,42 @@ const query = async function () {
       );
     });
   } catch (error) {}
+  listen();
 };
 
+const listen = function () {
+  DOMSelectors.searchform.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const quarry = DOMSelectors.SearchArea.value;
+    const searchQuery = async function () {
+      try {
+        const response2 = await fetch(
+          `https://api.spoonacular.com/food/menuItems/search?query=${quarry}&number=5&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+        );
+        const data2 = await response2.json();
+        console.log(data.menuItems);
+        data2.menuItems.forEach((recipe2) => {
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<div class="food-card">
+            <div class="food-card-front">
+                        <img src="${recipe2.image}" alt="" class="picture"/>
+                    </div>
+    
+                <div class="food-card-back">
+                    <h3 class="food-title">${recipe2.title}</h3>
+                    <div class="chain-container">
+                        <p class="restaurant-chain">Restaurant Chain:</p>
+                        <p class="restaurant-chain">${recipe2.restaurantChain}</p>
+                    </div>
+                </div>
+            </div>   
+            `
+          );
+        });
+      } catch (error) {}
+      searchQuery();
+    };
+  });
+};
 query();
