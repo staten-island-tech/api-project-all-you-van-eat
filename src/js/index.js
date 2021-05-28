1d4079310df60c69af9510854b78e24239c1d36e
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-05-27 23:05:04 -0400

    completed search
    cccccc

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 import { DOMSelectors } from "./DOM";
 
-//const key = "b9b841dbb44044dd9d0d26c2b377cec6";
+//const key = "da719eac9fa8411596f068053ad6f32a";
 
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/food/menuItems/search?query=All&number=72&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+      `https://api.spoonacular.com/food/menuItems/search?query=All&number=72&apiKey=da719eac9fa8411596f068053ad6f32a`
     );
     const data = await response.json();
     console.log(data.menuItems);
--- src/js/search.js
+++ src/js/search.js
@@ -8,7 +8,7 @@ const listen = function () {
     const searchQuery = async function () {
       try {
         const response = await fetch(
-          `https://api.spoonacular.com/food/menuItems/search?query=${quarry}&number=5&apiKey=b9b841dbb44044dd9d0d26c2b377cec6`
+          `https://api.spoonacular.com/food/menuItems/search?query=${quarry}&number=5&apiKey=da719eac9fa8411596f068053ad6f32a`
         );
         const data = await response.json();
         console.log(data.menuItems);
